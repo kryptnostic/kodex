5b67a8ffd7f3625bf25640cceee9e18bd5eee093
Sina Iman <sina@kryptnostic.com>
2014-08-22 15:24:25 -0700

    tidied up metadata request

--- src/main/java/com/kryptnostic/api/v1/models/request/MetadataRequest.java
+++ src/main/java/com/kryptnostic/api/v1/models/request/MetadataRequest.java
@@ -3,21 +3,22 @@ package com.kryptnostic.api.v1.models.request;
 import java.util.Collection;
 import java.util.Collections;
 
-import com.google.common.collect.Lists;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kryptnostic.api.v1.models.IndexableMetadata;
 
 public class MetadataRequest {
+    private static final String METADATA_PROPERTY = "metadata";
+
     private final Collection<IndexableMetadata> metadata;
 
-    public MetadataRequest() {
-        metadata = Lists.newArrayList();
+    @JsonCreator
+    public MetadataRequest(@JsonProperty(METADATA_PROPERTY) Collection<IndexableMetadata> metadata) {
+        this.metadata = metadata;
     }
 
+    @JsonProperty(METADATA_PROPERTY)
     public Collection<IndexableMetadata> getMetadata() {
         return Collections.unmodifiableCollection(metadata);
-    }
-
-    public void addMetadata(IndexableMetadata m) {
-        metadata.add(m);
     }
 }
--- src/main/java/com/kryptnostic/api/v1/models/request/SearchRequest.java
+++ src/main/java/com/kryptnostic/api/v1/models/request/SearchRequest.java
@@ -70,5 +70,4 @@ public class SearchRequest {
         return sortScore;
     }
 
-
 }
