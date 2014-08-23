de23f2472dc894ece3b497d8468030b05acbd94c
Nick Hewitt <nick@kryptnostic.com>
2014-08-22 20:59:50 -0700

    Added KryptnosticServicesFactory.

---
+++ src/main/java/com/kryptnostic/api/v1/client/KryptnosticServicesFactory.java
@@ -0,0 +1,29 @@
+package com.kryptnostic.api.v1.client;
+
+import com.kryptnostic.api.v1.indexing.IndexingService;
+import com.kryptnostic.api.v1.indexing.MetadataKeyService;
+
+/**
+ * KryptonsticServicesFactory to decouple services used in Kryptnostic
+ * client interfaces from the Kodex.
+ * 
+ * @author Nick Hewitt
+ */
+public interface KryptnosticServicesFactory {
+    /**
+     * @return KryptnosticStorage
+     */
+    KryptnosticStorage createStorageService();
+    /**
+     * @return KryptnosticSearch
+     */
+    KryptnosticSearch createSearchService();
+    /**
+     * @return MetadataKeyService
+     */
+    MetadataKeyService createMetadataKeyService();
+    /**
+     * @return IndexingService
+     */
+    IndexingService createIndexingService();
+}
