6b790b46923661f027a753e55333eff1be4d4aca
Nick Hewitt <nick@kryptnostic.com>
2014-08-26 16:54:39 -0700

    Refactor KryptnosticConnection into a KryptnosticContext, composed of a SearchService and a StorageService

--- src/main/java/com/kryptnostic/api/v1/search/SearchService.java
+++ src/main/java/com/kryptnostic/api/v1/search/SearchService.java
@@ -5,5 +5,10 @@ import java.util.Set;
 import com.kryptnostic.api.v1.indexing.metadata.Metadatum;
 
 public interface SearchService {
-	public Set<Metadatum> search( String query );
+    /**
+     * Submit search query on stored documents.
+     * 
+     * @return Set<Metadatum>, a collection of Metadatum associating the query with documents.
+     */
+    public Set<Metadatum> search(String query);
 }
--- src/main/java/com/kryptnostic/api/v1/storage/StorageService.java
+++ src/main/java/com/kryptnostic/api/v1/storage/StorageService.java
@@ -1,39 +1,34 @@
-package com.kryptnostic.api.v1.client;
+package com.kryptnostic.api.v1.storage;
 
 import com.kryptnostic.api.v1.exceptions.types.BadRequestException;
 import com.kryptnostic.api.v1.exceptions.types.ResourceNotFoundException;
-import com.kryptnostic.api.v1.models.SearchResult;
 
-public interface KryptnosticConnection {
+public interface StorageService {
     /**
      * Upload a document.
+     * 
      * @param document
      * @return ID of newly saved document
      * @throws BadRequestException
      */
-	String uploadDocument(String document) throws BadRequestException;
-	
-	/**
-	 * Update a document.
-	 * @param id
-	 * @param document
-	 * @return ID of newly saved document
-	 * @throws ResourceNotFoundException
-	 */
+    String uploadDocument(String document) throws BadRequestException;
+
+    /**
+     * Update a document.
+     * 
+     * @param id
+     * @param document
+     * @return ID of newly saved document
+     * @throws ResourceNotFoundException
+     */
     String updateDocument(String id, String document) throws ResourceNotFoundException;
-    
+
     /**
      * Retrieve a document's text.
+     * 
      * @param id
      * @return document text
      * @throws ResourceNotFoundException
      */
     String getDocument(String id) throws ResourceNotFoundException;
-    
-    /**
-     * Search for token.
-     * @param token
-     * @return search result
-     */
-    SearchResult search(String token);
 }
