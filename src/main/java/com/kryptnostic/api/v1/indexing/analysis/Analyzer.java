e75a620898349fa534136fac8e149ac5b017227f
Nick Hewitt <nick@kryptnostic.com>
2014-08-26 16:54:52 -0700

    added comment

--- src/main/java/com/kryptnostic/api/v1/indexing/analysis/Analyzer.java
+++ src/main/java/com/kryptnostic/api/v1/indexing/analysis/Analyzer.java
@@ -3,7 +3,10 @@ package com.kryptnostic.api.v1.indexing.analysis;
 import java.util.List;
 import java.util.Map;
 
-
 public interface Analyzer {
-	Map<String, List<Integer>> analyze( String source );
+    /**
+     * @return Map<String,List<Integer>>, a Map of tokens to a List of start indices for that token in the source
+     *         String.
+     */
+    Map<String, List<Integer>> analyze(String source);
 }
