e41aa0387ebf3b5e78649c326d6b65f435db398f
Nick Hewitt <nick@kryptnostic.com>
2014-08-27 14:15:27 -0700

    Refactor KryptnosticContext to KryptnosticClient, add new KryptnosticContext.

--- src/main/java/com/kryptnostic/kodex/v1/client/KryptnosticContext.java
+++ src/main/java/com/kryptnostic/kodex/v1/client/KryptnosticContext.java
@@ -1,8 +1,15 @@
 package com.kryptnostic.kodex.v1.client;
 
-import com.kryptnostic.search.v1.SearchService;
-import com.kryptnostic.storage.v1.StorageService;
+import com.kryptnostic.multivariate.gf2.SimplePolynomialFunction;
 
-public interface KryptnosticContext extends SearchService, StorageService {
-    
+/**
+ * KryptnosticContext is responsible for maintaining any shared state between the KryptnosticClient and Kryptnostic
+ * services.
+ * 
+ * @author Nick Hewitt &lt;nick@kryptnostic.com&gt;
+ *
+ */
+public interface KryptnosticContext {
+    SimplePolynomialFunction getIndexingHashFunction();
+
 }
