661eee3ed48d3f12361044476b1978300059d1e1
Nick Hewitt <nick@kryptnostic.com>
2014-08-27 14:27:28 -0700

    Added KryptnosticContext interface methods to track the search function and nonces.

--- src/main/java/com/kryptnostic/kodex/v1/client/KryptnosticContext.java
+++ src/main/java/com/kryptnostic/kodex/v1/client/KryptnosticContext.java
@@ -1,15 +1,25 @@
 package com.kryptnostic.kodex.v1.client;
 
+import java.util.List;
+
+import cern.colt.bitvector.BitVector;
+
 import com.kryptnostic.multivariate.gf2.SimplePolynomialFunction;
 
 /**
- * KryptnosticContext is responsible for maintaining any shared state between the KryptnosticClient and Kryptnostic
+ * KryptnosticContext is responsible for maintaining shared state between the KryptnosticClient and Kryptnostic
  * services.
  * 
  * @author Nick Hewitt &lt;nick@kryptnostic.com&gt;
  *
  */
 public interface KryptnosticContext {
-    SimplePolynomialFunction getIndexingHashFunction();
+    SimplePolynomialFunction getSearchFunction();
+
+    void setSearchFunction(SimplePolynomialFunction searchFunction);
+
+    List<BitVector> getNonces();
+
+    void addNonces(List<BitVector> nonces);
 
 }
