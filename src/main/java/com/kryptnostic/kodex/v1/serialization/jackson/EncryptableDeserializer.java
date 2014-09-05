a55f431e9aa041086d18e7b689929e4f7f82e56f
Sina Iman <sina@kryptnostic.com>
2014-09-05 13:55:36 -0700

    clean up deserializer, remove log4j

--- src/main/java/com/kryptnostic/kodex/v1/serialization/jackson/EncryptableDeserializer.java
+++ src/main/java/com/kryptnostic/kodex/v1/serialization/jackson/EncryptableDeserializer.java
@@ -6,16 +6,19 @@ import java.util.Iterator;
 
 import org.apache.commons.lang3.ArrayUtils;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.Lists;
 import com.hazelcast.util.Base64;
 import com.kryptnostic.crypto.Ciphertext;
 import com.kryptnostic.kodex.v1.models.Encryptable;
+import com.kryptnostic.kodex.v1.models.Encryptable.EncryptionScheme;
 import com.kryptnostic.kodex.v1.security.SecurityConfiguration;
 import com.kryptnostic.kodex.v1.security.SecurityConfigurationMapping;
 
@@ -33,39 +36,43 @@ public class EncryptableDeserializer extends JsonDeserializer<Encryptable> {
         ObjectCodec oc = jp.getCodec();
         JsonNode node = oc.readTree(jp);
 
-        Encryptable<?> encryptedObject = null;
-
-        // data
         JsonNode encryptedDataNode = node.get(Encryptable.FIELD_ENCRYPTED_DATA);
-        Collection<Long> dataLengths = Lists.newArrayList();
-        Iterator<JsonNode> iter = encryptedDataNode.get("length").iterator();
-        while (iter.hasNext()) {
-            JsonNode lenNode = iter.next();
-            dataLengths.add(lenNode.asLong());
-        }
-
-        // cls
         JsonNode encryptedClassNameNode = node.get(Encryptable.FIELD_ENCRYPTED_CLASS_NAME);
-        Collection<Long> classNameLengths = Lists.newArrayList();
-        Iterator<JsonNode> iterc = encryptedClassNameNode.get("length").iterator();
-        while (iterc.hasNext()) {
-            JsonNode lenNode = iterc.next();
-            classNameLengths.add(lenNode.asLong());
-        }
 
-        Ciphertext encryptedData = new Ciphertext(encryptedDataNode.get("contents").binaryValue(),
-                ArrayUtils.toPrimitive(dataLengths.toArray(new Long[0])));
-        Ciphertext encryptedClassName = new Ciphertext(encryptedClassNameNode.get("contents").binaryValue(),
-                ArrayUtils.toPrimitive(classNameLengths.toArray(new Long[0])));
-        Encryptable.EncryptionScheme scheme = Encryptable.EncryptionScheme.valueOf(node.get(Encryptable.FIELD_SCHEME)
-                .asText());
-        encryptedObject = new Encryptable(encryptedData, encryptedClassName, scheme);
+        Ciphertext encryptedData = deserializeCiphertext(encryptedDataNode);
+        Ciphertext encryptedClassName = deserializeCiphertext(encryptedClassNameNode);
+        Encryptable.EncryptionScheme scheme = deserializeEncryptionScheme(node);
 
-        Encryptable decryptedObject = null;
+        Encryptable<?> encryptedObject = new Encryptable(encryptedData, encryptedClassName, scheme);
 
         // decrypt if private key is available
+        return decryptIfPossible(encryptedObject);
+    }
+
+    private EncryptionScheme deserializeEncryptionScheme(JsonNode node) {
+        return Encryptable.EncryptionScheme.valueOf(node.get(Encryptable.FIELD_SCHEME).asText());
+    }
+
+    private Ciphertext deserializeCiphertext(JsonNode encryptedNode) throws IOException {
+        return new Ciphertext(encryptedNode.get("contents").binaryValue(), getLengthsArray(encryptedNode));
+    }
+
+    /**
+     * If a private key is available, decrypt the encryptedObject and return plain, otherwise return the still-encrypted object
+     * 
+     * This locates a private key based on a securityConfiguration
+     * 
+     * @param encryptedObject
+     * @return
+     * @throws JsonParseException
+     * @throws JsonMappingException
+     * @throws IOException
+     */
+    private Encryptable decryptIfPossible(Encryptable<?> encryptedObject) throws JsonParseException,
+            JsonMappingException, IOException {
+        Encryptable decryptedObject = null;
         if (securityConfiguration != null) {
-            SecurityConfiguration<?, ?> config = securityConfiguration.get(scheme);
+            SecurityConfiguration<?, ?> config = securityConfiguration.get(encryptedObject.getScheme());
             if (config != null) {
                 Object privateKey = config.getPrivateKey();
                 if (privateKey != null) {
@@ -79,7 +86,22 @@ public class EncryptableDeserializer extends JsonDeserializer<Encryptable> {
                 }
             }
         }
-
         return encryptedObject;
+    }
+
+    /**
+     * In deserializing a Ciphertext, this method reads the long array of lengths
+     * 
+     * @param encryptedNode
+     * @return
+     */
+    private long[] getLengthsArray(JsonNode encryptedNode) {
+        Collection<Long> lengths = Lists.newArrayList();
+        Iterator<JsonNode> iter = encryptedNode.get("length").iterator();
+        while (iter.hasNext()) {
+            JsonNode lenNode = iter.next();
+            lengths.add(lenNode.asLong());
+        }
+        return ArrayUtils.toPrimitive(lengths.toArray(new Long[0]));
     }
 }
