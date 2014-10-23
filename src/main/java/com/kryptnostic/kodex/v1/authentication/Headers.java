179a98f7344fe5a43a35efa6143d40bdfd296979
Sina Iman <sina@kryptnostic.com>
2014-10-23 04:40:54 -0700

    add some tools for authentication

--- src/main/java/com/kryptnostic/kodex/v1/authentication/Headers.java
+++ src/main/java/com/kryptnostic/kodex/v1/authentication/Headers.java
@@ -1,17 +1,14 @@
 package com.kryptnostic.kodex.v1.authentication;
 
-
 public enum Headers {
-    X_CSRF_TOKEN(HeaderNames.CSRF_TOKEN),
-    PRINCIPAL(HeaderNames.PRINCIPAL),
-    CREDENTIAL(HeaderNames.CREDENTIAL);
-    
+    X_CSRF_TOKEN( HeaderNames.CSRF_TOKEN ), PRINCIPAL( HeaderNames.PRINCIPAL ), CREDENTIAL( HeaderNames.CREDENTIAL );
+
     private final String header;
-    
+
     private Headers( String header ) {
         this.header = header;
     }
-    
+
     public String toString() {
         return header;
     }
--- src/main/java/com/kryptnostic/kodex/v1/authentication/PreauthenticationRequestInterceptor.java
+++ src/main/java/com/kryptnostic/kodex/v1/authentication/PreauthenticationRequestInterceptor.java
@@ -6,28 +6,28 @@ import com.kryptnostic.users.v1.UserKey;
 
 public class PreauthenticationRequestInterceptor implements RequestInterceptor {
     private UserKey user;
-    private String credential;
-    
+    private String  credential;
+
     public PreauthenticationRequestInterceptor() {
         this.user = null;
         this.credential = null;
     }
-    
-    public PreauthenticationRequestInterceptor( UserKey user , String credential ) {
+
+    public PreauthenticationRequestInterceptor( UserKey user, String credential ) {
         this.user = user;
         this.credential = credential;
     }
-    
-    public void updateCredentials( UserKey user , String credential ) {
+
+    public void updateCredentials( UserKey user, String credential ) {
         this.user = user;
         this.credential = credential;
     }
-    
+
     @Override
-    public void intercept(RequestFacade request) {
-        if( user!= null ) {
-            request.addHeader( Headers.PRINCIPAL.toString() , user.toString() );
-            request.addHeader( Headers.CREDENTIAL.toString() , credential );
+    public void intercept( RequestFacade request ) {
+        if ( user != null ) {
+            request.addHeader( Headers.PRINCIPAL.toString(), user.toString() );
+            request.addHeader( Headers.CREDENTIAL.toString(), credential );
         }
     }
 }
