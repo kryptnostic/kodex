60b498905527f4f677d15f7ef839aa32454dbad5
Sina Iman <sina@kryptnostic.com>
2014-10-24 02:48:41 -0700

    remove stuff from context interface

--- src/main/java/com/kryptnostic/kodex/v1/client/KryptnosticContext.java
+++ src/main/java/com/kryptnostic/kodex/v1/client/KryptnosticContext.java
@@ -1,13 +1,10 @@
 package com.kryptnostic.kodex.v1.client;
 
-import java.util.List;
-
 import cern.colt.bitvector.BitVector;
 
 import com.kryptnostic.kodex.v1.exceptions.types.ResourceNotFoundException;
 import com.kryptnostic.kodex.v1.security.SecurityService;
 import com.kryptnostic.multivariate.gf2.SimplePolynomialFunction;
-import com.kryptnostic.storage.v1.models.EncryptedSearchDocumentKey;
 
 /**
  * KryptnosticContext is responsible for maintaining shared state between the KryptnosticClient and Kryptnostic
@@ -17,9 +14,6 @@ import com.kryptnostic.storage.v1.models.EncryptedSearchDocumentKey;
  *
  */
 public interface KryptnosticContext {
-    List<EncryptedSearchDocumentKey> getDocumentKeys();
-
-    void addDocumentKeys( List<EncryptedSearchDocumentKey> keys );
 
     BitVector generateDocumentNonce();
 
