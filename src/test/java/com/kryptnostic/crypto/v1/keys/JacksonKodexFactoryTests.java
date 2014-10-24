82d22766e6c71737ab51cc244932ed6cf7e970f6
Nick Hewitt <nick@kryptnostic.com>
2014-10-24 00:34:06 -0700

    Jackson KodexFactory passes unit tests with FHE PrivateKey

---
+++ src/test/java/com/kryptnostic/crypto/v1/keys/JacksonKodexFactoryTests.java
@@ -0,0 +1,41 @@
+package com.kryptnostic.crypto.v1.keys;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kryptnostic.crypto.PrivateKey;
+import com.kryptnostic.kodex.v1.serialization.jackson.KodexObjectMapperFactory;
+
+public class JacksonKodexFactoryTests {
+    ObjectMapper mapper = (new KodexObjectMapperFactory()).getObjectMapper();
+    KodexMarshaller<PrivateKey> marshaller = new JacksonKodexFactory<PrivateKey>( mapper, PrivateKey.class );
+    private static final int PRIVATE_KEY_BLOCK_SIZE = 64;
+    PrivateKey object = new PrivateKey( PRIVATE_KEY_BLOCK_SIZE * 2, PRIVATE_KEY_BLOCK_SIZE);
+
+    @Test
+    public void testFromBytes( ) throws IOException {
+        byte[] bytes = mapper.writeValueAsBytes( object );
+        PrivateKey actual = marshaller.fromBytes( bytes );
+        Assert.assertEquals( object, actual );
+    }
+
+    @Test
+    public void testToBytes(  ) throws IOException {
+        
+        byte[] expected = mapper.writeValueAsBytes( object );
+        byte[] actual = marshaller.toBytes( object );   
+        Assert.assertTrue( Arrays.equals( expected, actual ) );
+    }
+    
+    @Test
+    public void testFromToBytes() throws IOException {
+        byte[] bytes = marshaller.toBytes( object );
+        PrivateKey actual = marshaller.fromBytes( bytes );
+        Assert.assertEquals( object, actual );
+    }
+
+}
