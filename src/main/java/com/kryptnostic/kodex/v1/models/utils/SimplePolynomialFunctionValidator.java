e120d0d62276be5c8b13e9ce597981b9108d0491
Matthew Tamayo <matthew@kryptnostic.com>
2014-10-28 01:34:50 -0700

    SPFV improvements.

--- src/main/java/com/kryptnostic/kodex/v1/models/utils/SimplePolynomialFunctionValidator.java
+++ src/main/java/com/kryptnostic/kodex/v1/models/utils/SimplePolynomialFunctionValidator.java
@@ -14,13 +14,14 @@ import com.kryptnostic.crypto.v1.keys.JacksonKodexMarshaller;
 import com.kryptnostic.multivariate.gf2.SimplePolynomialFunction;
 
 public class SimplePolynomialFunctionValidator implements Serializable {
-    private static final long serialVersionUID = 3325415662614137319L;
-    private static final int    DEFAULT_NUMBER_OF_ROUNDS = 1000;
-    private static final String INPUTS_FIELD            = "inputs";
-    private static final String OUTPUTS_FIELD           = "outputs";
-    private static final JacksonKodexMarshaller<SimplePolynomialFunctionValidator> marshaller = new JacksonKodexMarshaller<SimplePolynomialFunctionValidator>( SimplePolynomialFunctionValidator.class );
-    private final BitVector[]   inputs;
-    private final BitVector[]   outputs;
+    private static final long                                                      serialVersionUID         = 3325415662614137319L;
+    private static final int                                                       DEFAULT_NUMBER_OF_ROUNDS = 1000;
+    private static final String                                                    INPUTS_FIELD             = "inputs";
+    private static final String                                                    OUTPUTS_FIELD            = "outputs";
+    private static final JacksonKodexMarshaller<SimplePolynomialFunctionValidator> marshaller               = new JacksonKodexMarshaller<SimplePolynomialFunctionValidator>(
+                                                                                                                    SimplePolynomialFunctionValidator.class );
+    private final BitVector[]                                                      inputs;
+    private final BitVector[]                                                      outputs;
 
     @JsonCreator
     public SimplePolynomialFunctionValidator(
@@ -53,21 +54,21 @@ public class SimplePolynomialFunctionValidator implements Serializable {
     public BitVector[] getOutputs() {
         return outputs;
     }
-    
-    public boolean validate(SimplePolynomialFunction f ) {
-        for( int i = 0 ; i < inputs.length ; ++i ) {
-            if( !f.apply( inputs[i] ).equals( outputs[i ]) ) {
+
+    public boolean validate( SimplePolynomialFunction f ) {
+        for ( int i = 0; i < inputs.length; ++i ) {
+            if ( !f.apply( inputs[ i ] ).equals( outputs[ i ] ) ) {
                 return false;
             }
         }
         return true;
     }
-    
+
     @JsonIgnore
     public byte[] getBytes() throws IOException {
         return marshaller.toBytes( this );
     }
-    
+
     @Override
     public int hashCode() {
         final int prime = 31;
