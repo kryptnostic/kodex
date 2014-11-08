38b8808b09b04d6148ddf46bed6ddfd1ea8e298e
Sina Iman <sina@kryptnostic.com>
2014-11-07 19:34:10 -0800

    fix aesmeta test

--- src/test/java/com/kryptnostic/storage/v1/models/request/AesMetadataRequestTests.java
+++ src/test/java/com/kryptnostic/storage/v1/models/request/AesMetadataRequestTests.java
@@ -257,14 +257,14 @@ public class AesMetadataRequestTests extends AesEncryptableBase {
         data = data.encrypt( tmpKodex );
 
         // create the metadataRequest with our (ENCRYPTED) Encryptable
-        MetadataRequest req = new MetadataRequest( Arrays.asList( new IndexedMetadata( key, data, new DocumentId(
-                "test",
-                new UserKey( "test", "test" ) ) ) ) );
+        DocumentId docId = new DocumentId( "test", new UserKey( "test", "test" ) );
+        MetadataRequest req = new MetadataRequest( Arrays.asList( new IndexedMetadata( key, data, docId ) ) );
 
         String expected = "{\"metadata\":[{\"key\":" + wrapQuotes( BitVectors.marshalBitvector( key ) )
                 + ",\"data\":{\"" + Encryptable.FIELD_CLASS + "\":\"" + data.getClass().getCanonicalName() + "\",\""
                 + Encryptable.FIELD_ENCRYPTED_DATA + "\":" + serialize( data.getEncryptedData() ) + ",\""
-                + Encryptable.FIELD_ENCRYPTED_CLASS_NAME + "\":" + serialize( data.getEncryptedClassName() ) + "}}]}";
+                + Encryptable.FIELD_ENCRYPTED_CLASS_NAME + "\":" + serialize( data.getEncryptedClassName() )
+                + "},\"id\":" + serialize( docId ) + "}]}";
 
         String actual = serialize( req );
 
