c616cda8e980c67d56a577ccfa17120bd1126fa0
Nick Hewitt <nick@kryptnostic.com>
2015-02-10 21:22:17 -0800

    Revert "Make user key case-insensitive"
    This reverts commit e6e4873899e7a4371aa78a35370419cc1b374a75.

--- src/main/java/com/kryptnostic/directory/v1/models/UserKey.java
+++ src/main/java/com/kryptnostic/directory/v1/models/UserKey.java
@@ -1,12 +1,8 @@
 package com.kryptnostic.directory.v1.models;
 
-import static com.kryptnostic.kodex.v1.constants.Names.NAME_FIELD;
-import static com.kryptnostic.kodex.v1.constants.Names.REALM_FIELD;
-
 import java.io.Serializable;
 import java.util.List;
-import java.util.Locale;
-
+import static com.kryptnostic.kodex.v1.constants.Names.*;
 import org.apache.commons.lang3.StringUtils;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -14,26 +10,23 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
 
-public class UserKey implements Principal, Serializable {
-    private static final long       serialVersionUID    = 1392846078227265603L;
-    protected static final Splitter splitter            = Splitter.on( "." ).trimResults().omitEmptyStrings();
-    protected final String          name;
-    protected final String          realm;
-    public static final int         MAX_USERNAME_LENGTH = 1024;
-    public static final int         MAX_REALM_LENGTH    = 1024;
-
-    // TODO: Make Realm its own class to avoid confusion when calling API.
+public class UserKey implements Principal,Serializable {
+    private static final long serialVersionUID = 1392846078227265603L;
+    protected static final Splitter splitter = Splitter.on( "." ).trimResults().omitEmptyStrings();
+    protected final String name;
+    protected final String realm;
+    public static final int MAX_USERNAME_LENGTH = 1024;
+    public static final int MAX_REALM_LENGTH = 1024;
+    
+    //TODO: Make Realm its own class to avoid confusion when calling API.
     @JsonCreator
-    public UserKey( @JsonProperty( REALM_FIELD ) String realm, @JsonProperty( NAME_FIELD ) String name ) {
-        this.name = normalizeUsername( name );
+    public UserKey( 
+            @JsonProperty(REALM_FIELD) String realm , 
+            @JsonProperty(NAME_FIELD) String name ) {
+        this.name = name;
         this.realm = realm;
     }
-
-    // Username is case-insensitive
-    private static String normalizeUsername( String username ) {
-        return StringUtils.lowerCase( username, Locale.ROOT );
-    }
-
+    
     public String getName() {
         return name;
     }
@@ -44,9 +37,9 @@ public class UserKey implements Principal, Serializable {
 
     @Override
     public String toString() {
-        return buildFqn( realm, name );
+        return buildFqn( realm , name );
     }
-
+    
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -57,64 +50,58 @@ public class UserKey implements Principal, Serializable {
     }
 
     @Override
-    public boolean equals( Object obj ) {
-        if ( this == obj ) {
+    public boolean equals(Object obj) {
+        if (this == obj) {
             return true;
         }
-        if ( obj == null ) {
+        if (obj == null) {
             return false;
         }
-        if ( !( obj instanceof UserKey ) ) {
+        if (!( obj instanceof UserKey )) {
             return false;
         }
         UserKey other = (UserKey) obj;
-        if ( name == null ) {
-            if ( other.name != null ) {
+        if (name == null) {
+            if (other.name != null) {
                 return false;
             }
-        } else if ( !name.equals( other.name ) ) {
+        } else if (!name.equals( other.name )) {
             return false;
         }
-        if ( realm == null ) {
-            if ( other.realm != null ) {
+        if (realm == null) {
+            if (other.realm != null) {
                 return false;
             }
-        } else if ( !realm.equals( other.realm ) ) {
+        } else if (!realm.equals( other.realm )) {
             return false;
         }
         return true;
     }
 
     public static boolean isValidUsername( String name ) {
-        Preconditions.checkState(
-                StringUtils.isAlphanumeric( name ),
-                "Realm name cannot be blank and must consist of only alpha numeric characters." );
-        Preconditions.checkState( name.length() < MAX_USERNAME_LENGTH, "Username cannot be longer than "
-                + MAX_USERNAME_LENGTH + "characters" );
+        Preconditions.checkState( StringUtils.isAlphanumeric( name ) , "Realm name cannot be blank and must consist of only alpha numeric characters." );
+        Preconditions.checkState( name.length() < MAX_USERNAME_LENGTH , "Username cannot be longer than " + MAX_USERNAME_LENGTH + "characters" );
         return true;
     }
 
     public static boolean isValidRealm( String realm ) {
-        Preconditions.checkState(
-                StringUtils.isAlphanumeric( realm ),
-                "Realm name cannot be blank and must consist of only alpha numeric characters." );
-        Preconditions.checkState( realm.length() < MAX_REALM_LENGTH, "Realm name cannot be longer than "
-                + MAX_REALM_LENGTH + "characters" );
+        Preconditions.checkState( StringUtils.isAlphanumeric( realm ) , "Realm name cannot be blank and must consist of only alpha numeric characters." );
+        Preconditions.checkState( realm.length() < MAX_REALM_LENGTH , "Realm name cannot be longer than " + MAX_REALM_LENGTH + "characters" );
         return false;
     }
 
-    public static String buildFqn( String realm, String name ) {
-        return realm + "." + normalizeUsername( name );
+    public static String buildFqn( String realm , String name ) {
+        return realm + "." + name;
     }
-
+    
     public static UserKey fromString( final String fqn ) {
         List<String> parts = splitter.splitToList( fqn );
-        Preconditions.checkState( parts.size() == 2, "FQN should only yield two parts." );
+        Preconditions.checkState( parts.size() == 2 , "FQN should only yield two parts." );
         String realm = parts.get( 0 );
         String name = parts.get( 1 );
         isValidRealm( realm );
         isValidUsername( name );
-        return new UserKey( realm, name );
+        return new UserKey( realm , name );
     }
 
 }
