4321ca4d1c46ead1963f155a8322b58f6015bda5
Sina Iman <sina@kryptnostic.com>
2015-02-26 01:42:57 -0800

    remove cryptoServiceLoader from sharing client params bc its unnecessary since its in context

--- src/main/java/com/kryptnostic/sharing/v1/SharingClient.java
+++ src/main/java/com/kryptnostic/sharing/v1/SharingClient.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Set;
 
 import com.kryptnostic.directory.v1.models.UserKey;
-import com.kryptnostic.kodex.v1.crypto.keys.CryptoServiceLoader;
 import com.kryptnostic.kodex.v1.exceptions.types.ResourceNotFoundException;
 import com.kryptnostic.kodex.v1.exceptions.types.SecurityConfigurationException;
 import com.kryptnostic.storage.v1.models.EncryptedSearchObjectKey;
@@ -12,9 +11,9 @@ import com.kryptnostic.storage.v1.models.EncryptedSearchObjectKey;
 public interface SharingClient {
     void unshareObjectWithUsers( String objectId, Set<UserKey> users );
 
-    void shareObjectWithUsers( CryptoServiceLoader loader, String uuid, Set<UserKey> users );
+    void shareObjectWithUsers( String uuid, Set<UserKey> users ) throws ResourceNotFoundException;
 
-    int processIncomingShares( CryptoServiceLoader loader ) throws IOException, SecurityConfigurationException;
+    int processIncomingShares() throws IOException, SecurityConfigurationException;
 
     int getIncomingSharesCount();
 
