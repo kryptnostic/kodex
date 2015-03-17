e5c5b517501497a78cb530ab383626ba1c0e5aaa
Sina Iman <sina@kryptnostic.com>
2015-03-17 12:48:38 -0700

    remove spring dep

--- src/main/java/com/kryptnostic/directory/v1/exception/RealmMismatchException.java
+++ src/main/java/com/kryptnostic/directory/v1/exception/RealmMismatchException.java
@@ -1,11 +1,6 @@
 package com.kryptnostic.directory.v1.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(
-    value = HttpStatus.BAD_REQUEST,
-    reason = "Organization realm and request user realm do not match." )
 public class RealmMismatchException extends RuntimeException {
     private static final long serialVersionUID = 1544107975733654869L;
 }
--- src/main/java/com/kryptnostic/directory/v1/exception/UnauthorizedGroupRequestException.java
+++ src/main/java/com/kryptnostic/directory/v1/exception/UnauthorizedGroupRequestException.java
@@ -1,11 +1,6 @@
 package com.kryptnostic.directory.v1.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(
-    value = HttpStatus.BAD_REQUEST,
-    reason = "Requested includes unauthorized group." )
 public class UnauthorizedGroupRequestException extends RuntimeException {
     private static final long serialVersionUID = 1279438730669495588L;
 }
