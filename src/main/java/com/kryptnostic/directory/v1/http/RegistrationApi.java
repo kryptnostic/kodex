4a7e04a04ef873f7e697c46b9dd95165c5173496
Ryan Buckheit <ryan@kryptnostic.com>
2015-08-11 14:38:06 -0700

    fixes some of the broken API mappings

--- src/main/java/com/kryptnostic/directory/v1/http/RegistrationApi.java
+++ src/main/java/com/kryptnostic/directory/v1/http/RegistrationApi.java
@@ -5,27 +5,20 @@ import java.util.UUID;
 import retrofit.http.Body;
 import retrofit.http.POST;
 
-import com.kryptnostic.directory.v1.exception.DeveloperAlreadyExistsException;
-import com.kryptnostic.directory.v1.exception.InvalidEmailException;
-import com.kryptnostic.directory.v1.exception.MailException;
-import com.kryptnostic.directory.v1.exception.ReservationTakenException;
-import com.kryptnostic.directory.v1.model.DeveloperRegistration;
-import com.kryptnostic.directory.v1.model.request.DeveloperRegistrationRequest;
-import com.kryptnostic.kodex.v1.exceptions.types.BadRequestException;
 import com.kryptnostic.registration.v1.models.UserCreationRequest;
 
 public interface RegistrationApi {
-    String CONTROLLER = "/registration";
-    String DEVELOPERS = "/developers";
-    String USER       = "/user";
+    public static final String CONTROLLER = "/registration";
+    public static final String USER       = "/user";
+    public static final String DEVELOPERS = "/developers";
 
     /**
      * Registers a normal user using the domain of their e-mail as their domain.
-     * 
+     *
      * @param request
      * @return
      */
-    @POST( USER )
+    @POST( CONTROLLER + USER )
     UUID register( @Body UserCreationRequest request );
 
 }
