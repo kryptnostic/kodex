be8e3efb35369310081425b1fd661e0cc35d6290
Drew <drew@kryptnostic.com>
2015-09-04 21:18:20 -0700

    comment out future endpoints

--- src/main/java/com/kryptnostic/storage/v1/http/CryptoKeyStorageApi.java
+++ src/main/java/com/kryptnostic/storage/v1/http/CryptoKeyStorageApi.java
@@ -1,14 +1,11 @@
 package com.kryptnostic.storage.v1.http;
 
-import java.util.UUID;
-
 import com.kryptnostic.kodex.v1.exceptions.types.BadRequestException;
 import com.kryptnostic.kodex.v1.models.response.BasicResponse;
 
 import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.POST;
-import retrofit.http.Path;
 
 public interface CryptoKeyStorageApi {
     final String CONTROLLER         = "/keys";
@@ -37,12 +34,15 @@ public interface CryptoKeyStorageApi {
     @GET( CONTROLLER + HASH )
     byte[] getHashFunctionForCurrentUser() throws BadRequestException;
 
-    @GET( CONTROLLER + RSA_PUB )
-    byte[] getRsaPublicKeyForUser( @Path( ID ) UUID userId ) throws BadRequestException;
-
-    @POST( CONTROLLER + RSA_PUB )
-    BasicResponse<String> setRsaPublicKey( @Body byte[] key ) throws BadRequestException;
-
-    @GET( CONTROLLER + RSA_PUB )
-    byte[] getRsaPublicKeyForCurrentUser() throws BadRequestException;
+//
+//    For future use!
+//
+//    @GET( CONTROLLER + RSA_PUB )
+//    byte[] getRsaPublicKeyForUser( @Path( ID ) UUID userId ) throws BadRequestException;
+//
+//    @POST( CONTROLLER + RSA_PUB )
+//    BasicResponse<String> setRsaPublicKey( @Body byte[] key ) throws BadRequestException;
+//
+//    @GET( CONTROLLER + RSA_PUB )
+//    byte[] getRsaPublicKeyForCurrentUser() throws BadRequestException;
 }
