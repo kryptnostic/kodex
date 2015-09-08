8bd1b32574da054adfc149966f24c3583775c694
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-09-08 14:45:14 -0700

    Switch to block ciphertexts.

--- src/main/java/com/kryptnostic/storage/v1/http/CryptoKeyStorageApi.java
+++ src/main/java/com/kryptnostic/storage/v1/http/CryptoKeyStorageApi.java
@@ -1,10 +1,12 @@
 package com.kryptnostic.storage.v1.http;
 
-import com.google.common.base.Optional;
-import com.kryptnostic.kodex.v1.exceptions.types.BadRequestException;
 import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.POST;
+
+import com.google.common.base.Optional;
+import com.kryptnostic.kodex.v1.crypto.ciphers.BlockCiphertext;
+import com.kryptnostic.kodex.v1.exceptions.types.BadRequestException;
 
 public interface CryptoKeyStorageApi {
     final String CONTROLLER         = "/keys";
@@ -16,32 +18,32 @@ public interface CryptoKeyStorageApi {
     final String ID_PATH            = "/{" + ID + "}";
 
     @POST( CONTROLLER + PRIVATE )
-    Optional<String> setFHEPrivateKeyForCurrentUser( @Body byte[] key ) throws BadRequestException;
+    Optional<String> setFHEPrivateKeyForCurrentUser( @Body BlockCiphertext key ) throws BadRequestException;
 
     @GET( CONTROLLER + PRIVATE )
-    byte[] getFHEPrivateKeyForCurrentUser() throws BadRequestException;
+    BlockCiphertext getFHEPrivateKeyForCurrentUser() throws BadRequestException;
 
     @POST( CONTROLLER + SEARCH_PRIVATE )
-    Optional<String> setFHESearchPrivateKeyForCurrentUser( @Body byte[] key ) throws BadRequestException;
+    Optional<String> setFHESearchPrivateKeyForCurrentUser( @Body BlockCiphertext key ) throws BadRequestException;
 
     @GET( CONTROLLER + SEARCH_PRIVATE )
-    byte[] getFHESearchPrivateKeyForUser() throws BadRequestException;
+    BlockCiphertext getFHESearchPrivateKeyForUser() throws BadRequestException;
 
     @POST( CONTROLLER + HASH )
-    Optional<String> setHashFunctionForCurrentUser( @Body byte[] key ) throws BadRequestException;
+    Optional<String> setHashFunctionForCurrentUser( @Body BlockCiphertext key ) throws BadRequestException;
 
     @GET( CONTROLLER + HASH )
-    byte[] getHashFunctionForCurrentUser() throws BadRequestException;
+    BlockCiphertext getHashFunctionForCurrentUser() throws BadRequestException;
 
 //
 //    For future use!
 //
 //    @GET( CONTROLLER + RSA_PUB )
-//    byte[] getRsaPublicKeyForUser( @Path( ID ) UUID userId ) throws BadRequestException;
+//    BlockCiphertext getRsaPublicKeyForUser( @Path( ID ) UUID userId ) throws BadRequestException;
 //
 //    @POST( CONTROLLER + RSA_PUB )
-//    BasicResponse<String> setRsaPublicKey( @Body byte[] key ) throws BadRequestException;
+//    BasicResponse<String> setRsaPublicKey( @Body BlockCiphertext key ) throws BadRequestException;
 //
 //    @GET( CONTROLLER + RSA_PUB )
-//    byte[] getRsaPublicKeyForCurrentUser() throws BadRequestException;
+//    BlockCiphertext getRsaPublicKeyForCurrentUser() throws BadRequestException;
 }
