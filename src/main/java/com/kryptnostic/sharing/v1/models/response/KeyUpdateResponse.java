c4ab7ff00d4955c537b41e81bfce989eab6abb2a
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-09-12 16:40:19 -0700

    KeyUpdateResponse no longer serializable.

--- src/main/java/com/kryptnostic/sharing/v1/models/response/KeyUpdateResponse.java
+++ src/main/java/com/kryptnostic/sharing/v1/models/response/KeyUpdateResponse.java
@@ -1,13 +1,11 @@
 package com.kryptnostic.sharing.v1.models.response;
 
-import java.io.Serializable;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kryptnostic.kodex.v1.constants.Names;
 
-public final class KeyUpdateResponse implements Serializable {
-    private static final long serialVersionUID = 425136864440277920L;
+public final class KeyUpdateResponse  {
     private final Set<String> ids;
 
     public KeyUpdateResponse( @JsonProperty( Names.ID_FIELD ) Set<String> ids ) {
