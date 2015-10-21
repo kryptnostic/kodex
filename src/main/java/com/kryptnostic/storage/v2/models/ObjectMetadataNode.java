576a62097c8896adaf586b0fc7ceaf9012c49d92
Drew <drew@kryptnostic.com>
2015-10-21 15:10:16 -0700

    add a placeholder constructor

--- src/main/java/com/kryptnostic/storage/v2/models/ObjectMetadataNode.java
+++ src/main/java/com/kryptnostic/storage/v2/models/ObjectMetadataNode.java
@@ -1,9 +1,15 @@
 package com.kryptnostic.storage.v2.models;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class ObjectMetadataNode {
     Map<UUID, ObjectMetadataNode> children;
     ObjectMetadata            metadata;
+
+    public ObjectMetadataNode( ObjectMetadata metadata ) {
+        this.children = new HashMap<>();
+        this.metadata = metadata;
+    }
 }
