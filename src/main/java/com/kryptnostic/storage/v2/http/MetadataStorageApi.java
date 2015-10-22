29513567f7d19dd8d30ad0bbbc1adf81e00959bb
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-10-21 21:52:22 -0700

    Update metadata api.

--- src/main/java/com/kryptnostic/storage/v2/http/MetadataStorageApi.java
+++ src/main/java/com/kryptnostic/storage/v2/http/MetadataStorageApi.java
@@ -4,20 +4,22 @@ import java.util.Set;
 import java.util.UUID;
 
 import retrofit.client.Response;
+import retrofit.http.DELETE;
 import retrofit.http.POST;
 import retrofit.http.Path;
 
 import com.kryptnostic.storage.v2.models.PaddedMetadataObjectIds;
 
 public interface MetadataStorageApi {
-    String CONTROLLER       = "/metadata";
-    String OBJECT_ID        = "objectId";
-    String METADATA_ID      = "metadataId";
-    String DELETE           = "/delete";
+    String CONTROLLER     = "/metadata";
+    String OBJECT_ID      = "objectId";
+    String DELETE         = "/delete";
 
-    String OBJECT_ID_PATH   = "/{" + OBJECT_ID + "}";
-    String METADATA_ID_PATH = "/{" + METADATA_ID + "}";
+    String OBJECT_ID_PATH = "/{" + OBJECT_ID + "}";
 
     @POST( CONTROLLER + OBJECT_ID_PATH )
     Response createMetadataEntry( @Path( OBJECT_ID ) UUID objectId, Set<PaddedMetadataObjectIds> paddedMetadataObjectIds );
+
+    @DELETE( CONTROLLER + OBJECT_ID_PATH )
+    Response createMetadataEntry( @Path( OBJECT_ID ) UUID objectId );
 }
