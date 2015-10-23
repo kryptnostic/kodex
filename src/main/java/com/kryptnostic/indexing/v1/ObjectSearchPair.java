7c4d6a826b3d311acb261329c2400b8ed22581da
Nick Hewitt <nick@kryptnostic.com>
2015-10-23 16:27:45 -0700

    update JSON property name

--- src/main/java/com/kryptnostic/indexing/v1/ObjectSearchPair.java
+++ src/main/java/com/kryptnostic/indexing/v1/ObjectSearchPair.java
@@ -5,16 +5,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
 
 public class ObjectSearchPair {
-    public static final String INDEX_PAIR_FIELD = "indexPair";
+    public static final String SEARCH_PAIR_FIELD = "searchPair";
     private final byte[]       objectSearchPair;
 
     @JsonCreator
-    public ObjectSearchPair( @JsonProperty( INDEX_PAIR_FIELD ) byte[] objectSearchPair ) {
+    public ObjectSearchPair( @JsonProperty( SEARCH_PAIR_FIELD ) byte[] objectSearchPair ) {
         Preconditions.checkState( objectSearchPair.length == 2080, "Index pair must be 2080 bytes long." );
         this.objectSearchPair = objectSearchPair;
     }
 
-    @JsonProperty( INDEX_PAIR_FIELD )
+    @JsonProperty( SEARCH_PAIR_FIELD )
     public byte[] getSearchPair() {
         return objectSearchPair;
     }
