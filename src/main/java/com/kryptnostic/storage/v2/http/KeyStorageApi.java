5b25f613c9c0c23421f157afca3b77e3c5d2e3c6
Drew <drew@kryptnostic.com>
2015-11-04 15:13:20 -0800

    remove silly keyStorageApi class and update the new one

--- src/main/java/com/kryptnostic/storage/v2/http/KeyStorageApi.java
+++ src/main/java/com/kryptnostic/storage/v2/http/KeyStorageApi.java
@@ -19,9 +19,9 @@ public interface KeyStorageApi {
     static final String CONTROLLER           = "/keys";
     // FHE
     static final String FHE_PATH             = "/fhe";
-    static final String PRIVATE_KEY_PATH     = FHE_PATH + "/private";
-    static final String SEARCH_PRIVATE       = FHE_PATH + "/searchprivate";
-    static final String HASH                 = FHE_PATH + "/hash";
+    static final String FHE_PRIVATE_PATH     = FHE_PATH + "/private";
+    static final String FHE_SEARCH_PRIVATE   = FHE_PATH + "/searchprivate";
+    static final String FHE_HASH             = FHE_PATH + "/hash";
 
     static final String PUBLIC_KEY_PATH      = "/public";
     static final String RSA_PUBLIC           = "/rsa";
@@ -39,10 +39,10 @@ public interface KeyStorageApi {
     @POST( CONTROLLER + PUBLIC_KEY_PATH )
     void setPublicKey( @Body byte[] publicKey );
 
-    @GET( CONTROLLER + PRIVATE_KEY_PATH )
+    @GET( CONTROLLER + FHE_PRIVATE_PATH )
     BlockCiphertext getPrivateKey();
 
-    @POST( CONTROLLER + PRIVATE_KEY_PATH )
+    @POST( CONTROLLER + FHE_PRIVATE_PATH )
     void setPrivateKey( @Body BlockCiphertext encryptedPrivateKey );
 
     @GET( CONTROLLER + SALT_PATH + USER_ID_PATH )
@@ -62,25 +62,25 @@ public interface KeyStorageApi {
 
     Map<UUID, byte[]> getObjectCryptoServices( @Body Set<UUID> ids );
 
-    @POST( CONTROLLER + PRIVATE_KEY_PATH )
+    @POST( CONTROLLER + FHE_PRIVATE_PATH )
     Optional<String> setFHEPrivateKeyForCurrentUser( @Body BlockCiphertext key ) throws BadRequestException;
 
-    @GET( CONTROLLER + PRIVATE_KEY_PATH )
+    @GET( CONTROLLER + FHE_PRIVATE_PATH )
     Optional<BlockCiphertext> getFHEPrivateKeyForCurrentUser() throws BadRequestException;
 
-    @POST( CONTROLLER + SEARCH_PRIVATE )
+    @POST( CONTROLLER + FHE_SEARCH_PRIVATE )
     Optional<String> setFHESearchPrivateKeyForCurrentUser( @Body BlockCiphertext key ) throws BadRequestException;
 
-    @GET( CONTROLLER + SEARCH_PRIVATE )
+    @GET( CONTROLLER + FHE_SEARCH_PRIVATE )
     Optional<BlockCiphertext> getFHESearchPrivateKeyForUser() throws BadRequestException;
 
-    @POST( CONTROLLER + HASH )
+    @POST( CONTROLLER + FHE_HASH )
     Response setHashFunctionForCurrentUser( @Body byte[] key ) throws BadRequestException;
 
     /**
      * @return The byte level representation of the ClientHashFunction.
      * @throws BadRequestException
      */
-    @GET( CONTROLLER + HASH )
+    @GET( CONTROLLER + FHE_HASH )
     byte[] getHashFunctionForCurrentUser() throws BadRequestException;
 }
