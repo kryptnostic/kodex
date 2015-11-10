8e66939fb8f0a4884f8c64b1d528aaf2fb01c571
Drew <drew@kryptnostic.com>
2015-11-09 16:26:32 -0800

    fixed some unclear apis

--- src/main/java/com/kryptnostic/v2/storage/api/KeyStorageApi.java
+++ src/main/java/com/kryptnostic/v2/storage/api/KeyStorageApi.java
@@ -22,12 +22,13 @@ public interface KeyStorageApi {
     String CONTROLLER                     = "/keys";
     // FHE
     String FHE_PATH                       = "/fhe";
+    String FHE_PUBLIC_PATH               = FHE_PATH + "/public";
     String FHE_PRIVATE_PATH               = FHE_PATH + "/private";
     String FHE_SEARCH_PRIVATE             = FHE_PATH + "/searchprivate";
     String FHE_HASH                       = FHE_PATH + "/hash";
 
     String RSA_PATH                       = "/rsa";
-    String RSA_PUBLIC_KEY_PATH            = RSA_PATH + "/pub";
+    String RSA_PUBLIC_KEY_PATH            = RSA_PATH + "/public";
     String RSA_PRIVATE_KEY_PATH           = RSA_PATH + "/private";
 
     String SALT_PATH                      = "/salt";
@@ -55,13 +56,13 @@ public interface KeyStorageApi {
     BlockCiphertext getRSAPrivateKey();
 
     @POST( CONTROLLER + RSA_PUBLIC_KEY_PATH )
-    Optional<String> setPublicKey( @Body byte[] publicKey );
+    Optional<String> setRSAPublicKey( @Body byte[] publicKey );
 
     @GET( CONTROLLER + RSA_PUBLIC_KEY_PATH + USER_ID_PATH )
-    byte[] getPublicKey( @Path( USER ) UUID user );
+    byte[] getRSAPublicKey( @Path( USER ) UUID user );
 
     @POST( CONTROLLER + RSA_PUBLIC_KEY_PATH + BULK_PATH )
-    Map<UUID, byte[]> getPublicKeys( @Body Set<UUID> userIds );
+    Map<UUID, byte[]> getRSAPublicKeys( @Body Set<UUID> userIds );
 
     @POST( CONTROLLER + SALT_PATH + USER_ID_PATH )
     Optional<String> setEncryptedSalt( @Path( USER ) UUID user, @Body BlockCiphertext encryptedSalt );
@@ -105,6 +106,21 @@ public interface KeyStorageApi {
 
     @POST( CONTROLLER + VERSIONED_CRYPTO_SERVICES_PATH )
     Map<VersionedObjectKey, byte[]> getObjectCryptoServices( @Body VersionedObjectKeySet ids );
+
+    /**
+     *
+     * @param publicKey
+     */
+    @POST( CONTROLLER + FHE_PUBLIC_PATH )
+    void setFHEPublicKey( @Body byte[] publicKey );
+
+    /**
+     *
+     * @param user
+     * @return
+     */
+    @GET( CONTROLLER + FHE_PUBLIC_PATH + USER_ID_PATH )
+    byte[] getFHEPublicKey( @Path( USER ) UUID user );
 
     @POST( CONTROLLER + FHE_PRIVATE_PATH )
     Optional<String> setFHEPrivateKeyForCurrentUser( @Body BlockCiphertext key );
