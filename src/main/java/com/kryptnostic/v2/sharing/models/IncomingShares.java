1b095cefe2d9055cc6258c9ba100ba54f906207c
Drew <drew@kryptnostic.com>
2015-11-24 18:04:21 -0800

    incoming shares add default constructor

--- src/main/java/com/kryptnostic/v2/sharing/models/IncomingShares.java
+++ src/main/java/com/kryptnostic/v2/sharing/models/IncomingShares.java
@@ -8,6 +8,10 @@ public final class IncomingShares extends HashSet<Share> {
 
     private static final long serialVersionUID = -4163211276862529808L;
 
+    public IncomingShares() {
+        super();
+    }
+
     public IncomingShares( Set<Share> deserialize ) {
         super( deserialize );
     }
