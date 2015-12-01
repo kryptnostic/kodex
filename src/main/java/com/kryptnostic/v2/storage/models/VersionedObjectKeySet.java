a8698b45504c699eb154d37254e008e974de90e4
Drew <drew@kryptnostic.com>
2015-11-30 17:07:17 -0800

    micro optimizations

--- src/main/java/com/kryptnostic/v2/storage/models/VersionedObjectKeySet.java
+++ src/main/java/com/kryptnostic/v2/storage/models/VersionedObjectKeySet.java
@@ -3,6 +3,8 @@ package com.kryptnostic.v2.storage.models;
 import java.util.Collection;
 import java.util.HashSet;
 
+import com.kryptnostic.v2.utils.SetUtils;
+
 public class VersionedObjectKeySet extends HashSet<VersionedObjectKey>{
 
     private static final long serialVersionUID = -2280678803539884513L;
@@ -14,9 +16,9 @@ public class VersionedObjectKeySet extends HashSet<VersionedObjectKey>{
     public VersionedObjectKeySet( Collection<? extends VersionedObjectKey> c ) {
         super( c );
     }
-    
+
     public VersionedObjectKeySet( int i ) {
-        super( i );
+        super( SetUtils.expectedSize( i ) );
     }
 
     public VersionedObjectKeySet( VersionedObjectKey key ) {
