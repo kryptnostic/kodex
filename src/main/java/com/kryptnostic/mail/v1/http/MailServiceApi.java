631b42ac0c368115d310afc24b837c24c2cea8b0
Yao <yao@kryptnostic.com>
2015-12-07 17:25:02 -0800

    throw exceptions

--- src/main/java/com/kryptnostic/mail/v1/http/MailServiceApi.java
+++ src/main/java/com/kryptnostic/mail/v1/http/MailServiceApi.java
@@ -1,16 +1,18 @@
 package com.kryptnostic.mail.v1.http;
 
+import java.io.IOException;
+
 import com.kryptnostic.kodex.v1.models.response.BasicResponse;
 import com.kryptnostic.mail.v1.models.EmailRequest;
+
 import retrofit.http.Body;
 import retrofit.http.POST;
 
-
 public interface MailServiceApi {
 
-  String INVITE = "invite";
+    String INVITE = "invite";
 
-  @POST(INVITE)
-  BasicResponse<String> sendInvitationEmail(@Body EmailRequest request);
+    @POST( INVITE )
+    BasicResponse<String> sendInvitationEmail( @Body EmailRequest request ) throws InterruptedException, IOException;
 
 }
