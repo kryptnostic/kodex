9dbbc553773c47bce25e668eb1ae6b569695c861
Nick Hewitt <nick@kryptnostic.com>
2015-12-09 09:50:48 -0800

    Add controller constant and "/" to invite constant

--- src/main/java/com/kryptnostic/mail/v1/http/MailServiceApi.java
+++ src/main/java/com/kryptnostic/mail/v1/http/MailServiceApi.java
@@ -1,14 +1,14 @@
 package com.kryptnostic.mail.v1.http;
 
-import com.kryptnostic.kodex.v1.models.response.BasicResponse;
-import com.kryptnostic.mail.v1.models.EmailRequest;
-
 import retrofit.http.Body;
 import retrofit.http.POST;
 
-public interface MailServiceApi {
+import com.kryptnostic.kodex.v1.models.response.BasicResponse;
+import com.kryptnostic.mail.v1.models.EmailRequest;
 
-    String INVITE = "invite";
+public interface MailServiceApi {
+    String CONTROLLER = "/mail";
+    String INVITE     = "/invite";
 
     @POST( INVITE )
     BasicResponse<String> sendInvitationEmail( @Body EmailRequest request );
