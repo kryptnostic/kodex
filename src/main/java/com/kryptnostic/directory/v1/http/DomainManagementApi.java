eff9e053403000a6cb6d9f9e5d0bb1d3cb942281
Yao <yao@kryptnostic.com>
2015-12-16 17:10:10 -0800

    add domain trusted white list api

--- src/main/java/com/kryptnostic/directory/v1/http/DomainManagementApi.java
+++ src/main/java/com/kryptnostic/directory/v1/http/DomainManagementApi.java
@@ -2,20 +2,23 @@ package com.kryptnostic.directory.v1.http;
 
 import java.util.UUID;
 
-import retrofit.client.Response;
-import retrofit.http.Body;
-import retrofit.http.GET;
-import retrofit.http.POST;
-import retrofit.http.Path;
-
 import com.codahale.metrics.annotation.Timed;
 import com.google.common.base.Optional;
 import com.kryptnostic.directory.v1.model.DomainUpdate;
+
+import retrofit.client.Response;
+import retrofit.http.Body;
+import retrofit.http.DELETE;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.PUT;
+import retrofit.http.Path;
 
 /**
  * {@code Domain} management functionality.
  * 
  * @author Nick Hewitt
+ * @author Yao Pan
  *
  */
 public interface DomainManagementApi {
@@ -23,6 +26,8 @@ public interface DomainManagementApi {
     public static final String NAME          = "name";
     public static final String NAME_PATH     = "/{" + NAME + "}";
     public static final String NAME_WITH_DOT = "/{" + NAME + ":.+}";
+
+    public static final String WHITE_LIST    = "/whitelist";
 
     /**
      * Update domain settings.
@@ -36,6 +41,23 @@ public interface DomainManagementApi {
 
     @Timed
     @GET( DOMAIN + NAME_PATH )
-    Optional<UUID> getDomainId( @Path( NAME ) String domainName );
+    Optional<UUID> getDomainId( @Path( NAME ) String domainName);
 
+    /**
+     * Update white list settings.
+     * 
+     * @param domains
+     * @return
+     */
+    @Timed
+    @PUT( WHITE_LIST + NAME_PATH )
+    Response addDomainWhiteList( @Path( NAME ) String domainName, @Body String[] domains);
+
+    @Timed
+    @GET( WHITE_LIST + NAME_PATH )
+    String[] getDomainWhiteList( @Path( NAME ) String domainName);
+
+    @Timed
+    @DELETE( WHITE_LIST + NAME_PATH )
+    Response deleteDomainWhiteList( @Path( NAME ) String domainName, @Body String[] domains);
 }
