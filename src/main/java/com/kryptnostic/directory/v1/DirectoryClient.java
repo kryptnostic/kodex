8a6aa75e09ad3684e33ef2ad4d1906aa5fbc756a
Drew <drew@kryptnostic.com>
2015-12-17 19:32:08 -0800

    use iterable instead of set

--- src/main/java/com/kryptnostic/directory/v1/DirectoryClient.java
+++ src/main/java/com/kryptnostic/directory/v1/DirectoryClient.java
@@ -1,6 +1,5 @@
 package com.kryptnostic.directory.v1;
 
-import java.util.Set;
 import java.util.UUID;
 
 import com.kryptnostic.directory.v1.model.response.PublicKeyEnvelope;
@@ -8,7 +7,7 @@ import com.kryptnostic.kodex.v1.exceptions.types.ResourceNotFoundException;
 import com.kryptnostic.sharing.v1.models.NotificationPreference;
 
 public interface DirectoryClient {
-    Set<UUID> listUsersInRealm( String realm );
+    Iterable<UUID> listUsersInRealm( String realm );
 
     PublicKeyEnvelope getPublicKey( UUID id ) throws ResourceNotFoundException;
 
