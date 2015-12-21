1eee545c30c0dd686edadd818fa7ec8dff9089ed
Drew <drew@kryptnostic.com>
2015-12-21 13:52:55 -0800

    include controller in api interface paths

--- src/main/java/com/kryptnostic/v2/api/ObjectAuthorizationApi.java
+++ src/main/java/com/kryptnostic/v2/api/ObjectAuthorizationApi.java
@@ -22,13 +22,13 @@ public interface ObjectAuthorizationApi {
     String FULL_WRITERS_PATH = WRITERS_PATH + OBJECT_ID_PATH;
     String FULL_OWNERS_PATH  = OWNERS_PATH + OBJECT_ID_PATH;
 
-    @GET( FULL_READERS_PATH )
+    @GET( CONTROLLER + FULL_READERS_PATH )
     Iterable<UUID> getUsersWithReadAccess( @Path( ID ) UUID objectId);
 
-    @GET( FULL_WRITERS_PATH )
+    @GET( CONTROLLER + FULL_WRITERS_PATH )
     Iterable<UUID> getUsersWithWriteAccess( @Path( ID ) UUID objectId);
 
-    @GET( FULL_OWNERS_PATH )
+    @GET( CONTROLLER + FULL_OWNERS_PATH )
     Iterable<UUID> getUsersWithOwnerAccess( @Path( ID ) UUID objectId);
 
 }
