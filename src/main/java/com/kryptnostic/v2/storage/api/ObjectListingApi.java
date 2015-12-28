097c859c868cbd1b0e092c465d7293cf94b4275e
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2015-12-28 11:25:02 -0800

    Added in VersionedObjectKeys calls

--- src/main/java/com/kryptnostic/v2/storage/api/ObjectListingApi.java
+++ src/main/java/com/kryptnostic/v2/storage/api/ObjectListingApi.java
@@ -4,12 +4,13 @@ import java.util.Set;
 import java.util.UUID;
 
 import com.kryptnostic.v2.constants.Names;
+import com.kryptnostic.v2.storage.models.VersionedObjectKey;
 
 import retrofit.http.GET;
 import retrofit.http.Path;
 
 /**
- * This API is used for retrieving paged lists of objects for a user. Ordering is not guaranteed across calls. 
+ * This API is used for retrieving paged lists of objects for a user. Ordering is not guaranteed across calls.
  * 
  * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt;
  *
@@ -44,6 +45,9 @@ public interface ObjectListingApi {
             @Path( PAGE_SIZE ) Integer pageSize );
 
     @GET( CONTROLLER + USER_ID_PATH + TYPE_ID_PATH )
+    Set<VersionedObjectKey> getVersionedObjectKeysByType( @Path( ID ) UUID userId, @Path( TYPE ) UUID type );
+
+    @GET( CONTROLLER + USER_ID_PATH + TYPE_ID_PATH )
     Set<UUID> getObjectIdsByType( @Path( ID ) UUID userId, @Path( TYPE ) UUID type );
 
     @GET( CONTROLLER + USER_ID_PATH + TYPE_ID_PATH + PAGE_SIZE_PATH + PAGE_PATH )
@@ -51,8 +55,15 @@ public interface ObjectListingApi {
             @Path( ID ) UUID userId,
             @Path( TYPE ) UUID typeId,
             @Path( PAGE ) Integer offset,
+            @Path( PAGE_SIZE ) Integer pageSize);
+
+    @GET( CONTROLLER + USER_ID_PATH + TYPE_ID_PATH + PAGE_SIZE_PATH + PAGE_PATH )
+    Set<VersionedObjectKey> getVersionedObjectKeysByTypePaged(
+            @Path( ID ) UUID userId,
+            @Path( TYPE ) UUID typeId,
+            @Path( PAGE ) Integer offset,
             @Path( PAGE_SIZE ) Integer pageSize );
 
     @GET( CONTROLLER + TYPE_NAME_PATH )
-    UUID getTypeForName( @Path( TYPE ) String typeName);
+    UUID getTypeForName( @Path( TYPE ) String typeName );
 }
