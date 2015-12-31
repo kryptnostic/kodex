b656f53ebb1d481a20f6ca86a692308114533bc6
Yao <yao@kryptnostic.com>
2015-12-30 23:02:19 -0800

    get rid of mail service related packages, moved to heracles

--- src/test/java/com/kryptnostic/mail/v1/models/EmailRequestTest.java
+++ src/test/java/com/kryptnostic/mail/v1/models/EmailRequestTest.java
@@ -3,6 +3,7 @@ package com.kryptnostic.mail.v1.models;
 import org.junit.Test;
 
 import com.google.common.base.Optional;
+import com.kryptnostic.mail.models.EmailRequest;
 import com.kryptnostic.utils.BaseJacksonSerializationTest;
 
 public class EmailRequestTest extends BaseJacksonSerializationTest<EmailRequest> {
