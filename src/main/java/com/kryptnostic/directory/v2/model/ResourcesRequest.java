277601a7cb973f4eda3dabf8abfcef236ee24411
Yao <yao@kryptnostic.com>
2016-01-28 10:50:40 -0800

    refactor LinkedHashMap to HashMap

--- src/main/java/com/kryptnostic/directory/v2/model/ResourcesRequest.java
+++ src/main/java/com/kryptnostic/directory/v2/model/ResourcesRequest.java
@@ -1,11 +1,11 @@
 package com.kryptnostic.directory.v2.model;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.google.common.collect.ImmutableMap;
 
-public class ResourcesRequest extends LinkedHashMap<String, Long> {
+public class ResourcesRequest extends HashMap<String, Long> {
     private static final long serialVersionUID = 2373322922110759415L;
 
     public ResourcesRequest() {
