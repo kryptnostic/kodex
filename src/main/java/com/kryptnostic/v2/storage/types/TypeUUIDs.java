39a834f38512f48ec565c9db17018ef8acf68b1c
Elliott Jin <elliott@kryptnostic.com>
2016-01-29 11:22:36 -0800

    Oops, avoid collision in type UUID

--- src/main/java/com/kryptnostic/v2/storage/types/TypeUUIDs.java
+++ src/main/java/com/kryptnostic/v2/storage/types/TypeUUIDs.java
@@ -12,8 +12,6 @@ public final class TypeUUIDs {
     public static final UUID UTF8_STRING            = new UUID( 0L, 1L );
     public static final UUID UTF16_STRING           = new UUID( 0L, 2L );
     public static final UUID INDEX_METADATA         = new UUID( 0L, 3L );
-    public static final UUID INDEX_SEGMENT          = new UUID( 0L, 4L );
-
 
     /**
      * A block is a reserved type for chunking a large object into several child objects.
@@ -21,6 +19,8 @@ public final class TypeUUIDs {
     public static final UUID BLOCK                  = new UUID( 0L, 4L );
     public static final UUID FHE_PRIVATE_KEY        = new UUID( 0L, 5L );
     public static final UUID FHE_SEARCH_PRIVATE_KEY = new UUID( 0L, 6L );
+
+    public static final UUID INDEX_SEGMENT          = new UUID( 0L, 7L );
 
     public static final UUID DEFAULT_TYPE           = UTF8_STRING;
 }
