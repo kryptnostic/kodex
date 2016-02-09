c815f0f2ad1b0c518fcd3f0868e55fc3e2c4effc
Drew <drew@kryptnostic.com>
2016-02-09 14:43:25 -0800

    just use RhizomeUtils.(de)serializeEnumSet

--- src/main/java/com/kryptnostic/mapstores/v2/Permission.java
+++ src/main/java/com/kryptnostic/mapstores/v2/Permission.java
@@ -1,27 +1,5 @@
 package com.kryptnostic.mapstores.v2;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
-import java.util.EnumSet;
-import java.util.Set;
-
 public enum Permission {
     READ, WRITE, OWNER;
-
-    public static void serializeSet( DataOutput out, Set<Permission> set ) throws IOException {
-        for ( Permission permission : Permission.values() ) {
-            out.writeBoolean( set.contains( permission ) );
-        }
-    }
-
-    public static Set<Permission> deserializeSet( DataInput in) throws IOException {
-        EnumSet<Permission> permissions = EnumSet.noneOf( Permission.class );
-        for ( Permission permission : Permission.values() ) {
-            if ( in.readBoolean() ) {
-                permissions.add( permission );
-            }
-        }
-        return permissions;
-    }
 }
