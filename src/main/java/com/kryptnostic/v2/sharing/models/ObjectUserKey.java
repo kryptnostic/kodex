d32343de7a5a1555860b307c7b7befe7bd1e45e9
Drew <drew@kryptnostic.com>
2016-02-10 17:45:26 -0800

    add hashcode, equals, compareTo

--- src/main/java/com/kryptnostic/v2/sharing/models/ObjectUserKey.java
+++ src/main/java/com/kryptnostic/v2/sharing/models/ObjectUserKey.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kryptnostic.kodex.v1.constants.Names;
 
-public class ObjectUserKey {
+public class ObjectUserKey implements Comparable<ObjectUserKey> {
     private final UUID         objectId;
     private final UUID         userId;
 
@@ -27,6 +27,47 @@ public class ObjectUserKey {
     @JsonProperty( Names.USER_FIELD )
     public UUID getUserId() {
         return userId;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( objectId == null ) ? 0 : objectId.hashCode() );
+        result = prime * result + ( ( userId == null ) ? 0 : userId.hashCode() );
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals( Object obj ) {
+        if ( this == obj ) return true;
+        if ( obj == null ) return false;
+        if ( getClass() != obj.getClass() ) return false;
+        ObjectUserKey other = (ObjectUserKey) obj;
+        if ( objectId == null ) {
+            if ( other.objectId != null ) return false;
+        } else if ( !objectId.equals( other.objectId ) ) return false;
+        if ( userId == null ) {
+            if ( other.userId != null ) return false;
+        } else if ( !userId.equals( other.userId ) ) return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo( ObjectUserKey o ) {
+        int userIdCompare = userId.compareTo( o.userId );
+        if ( userIdCompare == 0 ) {
+            return objectId.compareTo( o.objectId );
+        }
+        return userIdCompare;
     }
 
 }
