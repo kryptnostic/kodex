d1a7461376bc3126f2a548302817d36abe9460e7
Elliott Jin <elliott@kryptnostic.com>
2016-02-12 18:07:04 -0800

    Specify separate blacklist for each root when loading metadata tree

--- src/main/java/com/kryptnostic/v2/storage/models/ObjectTreeLoadRequest.java
+++ src/main/java/com/kryptnostic/v2/storage/models/ObjectTreeLoadRequest.java
@@ -10,14 +10,13 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import com.kryptnostic.v2.constants.Names;
 
 @Immutable
 public class ObjectTreeLoadRequest {
     private static final int                    DEFAULT_DEPTH = 0;
     private final Set<UUID>                     objectIds;
-    private final Set<VersionedObjectKey>       objectIdsToFilter;
+    private final Map<UUID, Set<UUID>>          objectIdsToFilter;
     private final Map<UUID, Set<LoadLevel>>     typeLoadLevels;
     private final Map<UUID, VersionedObjectKey> createdAfter;
     private final int                           loadDepth;
@@ -25,7 +24,7 @@ public class ObjectTreeLoadRequest {
     public ObjectTreeLoadRequest( Set<UUID> objectIds, Map<UUID, Set<LoadLevel>> typeLoadLevels ) {
         this(
                 objectIds,
-                Optional.<Set<VersionedObjectKey>> absent(),
+                Optional.<Map<UUID, Set<UUID>>> absent(),
                 typeLoadLevels,
                 Optional.<Map<UUID, VersionedObjectKey>> absent(),
                 Optional.of( DEFAULT_DEPTH ) );
@@ -34,12 +33,12 @@ public class ObjectTreeLoadRequest {
     @JsonCreator
     public ObjectTreeLoadRequest(
             @JsonProperty( Names.OBJECT_IDS_FIELD ) Set<UUID> objectIds,
-            @JsonProperty( Names.OBJECTS_FILTER_FIELD ) Optional<Set<VersionedObjectKey>> objectIdsToFilter,
+            @JsonProperty( Names.OBJECTS_FILTER_FIELD ) Optional<Map<UUID, Set<UUID>>> objectIdsToFilter,
             @JsonProperty( Names.LOAD_LEVELS_FIELD ) Map<UUID, Set<LoadLevel>> typeLoadLevels,
             @JsonProperty( Names.CREATED_AFTER_FIELD ) Optional<Map<UUID, VersionedObjectKey>> createdAfter,
             @JsonProperty( Names.DEPTH_FIELD ) Optional<Integer> loadDepth) {
         this.objectIds = objectIds;
-        this.objectIdsToFilter = objectIdsToFilter.or( ImmutableSet.<VersionedObjectKey> of() );
+        this.objectIdsToFilter = objectIdsToFilter.or( ImmutableMap.<UUID, Set<UUID>> of() );
         this.typeLoadLevels = typeLoadLevels;
         this.createdAfter = createdAfter.or( ImmutableMap.<UUID, VersionedObjectKey> of() );
         this.loadDepth = loadDepth.or( DEFAULT_DEPTH );
@@ -51,7 +50,7 @@ public class ObjectTreeLoadRequest {
     }
 
     @JsonProperty( Names.OBJECTS_FILTER_FIELD )
-    public Set<VersionedObjectKey> getObjectIdsToFilter() {
+    public Map<UUID, Set<UUID>> getObjectIdsToFilter() {
         return objectIdsToFilter;
     }
 
