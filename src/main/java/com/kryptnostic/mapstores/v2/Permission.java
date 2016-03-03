415556bac46496eba1a2d7512ca7034a90d2e07f
Drew <makosblade@gmail.com>
2016-03-02 19:38:04 -0800

    notes on Permission enum

--- src/main/java/com/kryptnostic/mapstores/v2/Permission.java
+++ src/main/java/com/kryptnostic/mapstores/v2/Permission.java
@@ -1,5 +1,11 @@
 package com.kryptnostic.mapstores.v2;
 
+/**
+ * NOTE: DO NOT EVER RE-ORDER THESE ENTRIES. We currently rely on .ordinal and .values() to serialize and changing the
+ * order changes these values.
+ * 
+ * @author Drew Bailey drew@kryptnostic.com
+ */
 public enum Permission {
     READ, WRITE, OWNER;
 }
