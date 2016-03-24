ed40745310c7ed34b0e0fc9139232fb83f2f3021
Drew Bailey <drew@kryptnostic.com>
2016-03-24 16:42:41 -0700

    update sharingapi

--- src/main/java/com/kryptnostic/v2/sharing/api/SharingApi.java
+++ src/main/java/com/kryptnostic/v2/sharing/api/SharingApi.java
@@ -20,42 +20,41 @@ import retrofit.http.Path;
 
 public interface SharingApi {
 
-    String ID      = Names.ID_FIELD;
-    String VERSION = Names.VERSION_FIELD;
+    String CONTROLLER     = "/share";
+    String ID             = Names.ID_FIELD;
+    String VERSION        = Names.VERSION_FIELD;
 
     String OBJECT_ID_PATH = "/id/{" + ID + "}";
     String VERSION_PATH   = "/{" + VERSION + "}";
 
-    String SHARE       = "/share";
-    String OBJECT      = "/object";
-    String REVOKE      = "/revoke";
-    String KEYS        = "/keys";
-    String OBJECT_KEYS = "/objectKeys";
+    String OBJECT_PATH    = "/object";
+    String KEYS           = "/keys";
+    String REMOVE_INCOMING_SHARE_PATH = OBJECT_PATH + OBJECT_ID_PATH + VERSION_PATH;
 
-    @GET( SHARE + OBJECT )
+    @GET( CONTROLLER + OBJECT_PATH )
     Iterable<Share> getIncomingShares();
 
-    @DELETE( SHARE + OBJECT + OBJECT_ID_PATH + VERSION_PATH )
+    @DELETE( CONTROLLER + REMOVE_INCOMING_SHARE_PATH )
     Response removeIncomingShare( @Path( ID ) UUID objectKey, @Path( VERSION ) long version);
 
-    @POST( SHARE + OBJECT  )
+    @POST( CONTROLLER + OBJECT_PATH )
     Response share( @Body SharingRequest request );
 
-    @DELETE( SHARE + OBJECT )
+    @DELETE( CONTROLLER + OBJECT_PATH )
     Response revokeAccess( @Body RevocationRequest request );
 
     // TODO: Consider creating objects here.
-    @POST( SHARE + KEYS )
+    @POST( CONTROLLER + KEYS )
     Response addSearchPairs( @Body Set<VersionedObjectSearchPair> versionedObjectSearchPairs );
 
-    @DELETE( SHARE + KEYS )
+    @DELETE( CONTROLLER + KEYS )
     Response removeSearchPairs( @Body Set<VersionedObjectKey> uuids );
 
     // TODO: Once versions exceed values supported by JS ~2^54 we will have problems if ecmascript hasn't caught up by
-    @PUT( SHARE + KEYS + OBJECT_ID_PATH + VERSION_PATH )
+    @PUT( CONTROLLER + KEYS + OBJECT_ID_PATH + VERSION_PATH )
     Response addSearchPair( @Path( ID ) UUID objectId, @Path( VERSION ) long version, @Body byte[] objectSearchPair);
 
-    @GET( SHARE + KEYS + OBJECT_ID_PATH + VERSION_PATH )
+    @GET( CONTROLLER + KEYS + OBJECT_ID_PATH + VERSION_PATH )
     byte[] getSearchPair( @Path( ID ) UUID id, @Path( VERSION ) long version);
 
 }
