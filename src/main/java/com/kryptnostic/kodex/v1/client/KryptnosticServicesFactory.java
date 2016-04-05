cd38a54a8fb4ee873feaf4b3f7b57fa9c272aae5
Yao <yao@kryptnostic.com>
2016-04-04 17:52:10 -0700

    remove DirectoryApi

--- src/main/java/com/kryptnostic/kodex/v1/client/KryptnosticServicesFactory.java
+++ src/main/java/com/kryptnostic/kodex/v1/client/KryptnosticServicesFactory.java
@@ -1,6 +1,6 @@
 package com.kryptnostic.kodex.v1.client;
 
-import com.kryptnostic.directory.v1.http.DirectoryApi;
+import com.kryptnostic.directory.v1.http.UserDirectoryApi;
 import com.kryptnostic.sharing.v1.http.SharingApi;
 
 /**
@@ -10,6 +10,6 @@ public interface KryptnosticServicesFactory {
 
     SharingApi createSharingApi();
 
-    DirectoryApi createDirectoryApi();
+    UserDirectoryApi createDirectoryApi();
 
 }
