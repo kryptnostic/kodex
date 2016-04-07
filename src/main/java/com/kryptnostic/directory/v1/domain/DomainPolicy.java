7018aa535bb661ca898f99d0355145ec9dd323a1
Drew Bailey <drew@kryptnostic.com>
2016-04-07 16:58:48 -0700

    Revert "update domain sharing policy related api"
    This reverts commit 37da6bd7f22a26016cca78a152b4b944aacd9819.

--- src/main/java/com/kryptnostic/directory/v1/domain/DomainPolicy.java
+++
@@ -1,7 +0,0 @@
-package com.kryptnostic.directory.v1.domain;
-
-public enum DomainPolicy {
-    Public,
-    Discoverable,
-    Private;
-}
--- src/main/java/com/kryptnostic/directory/v1/http/DomainManagementApi.java
+++ src/main/java/com/kryptnostic/directory/v1/http/DomainManagementApi.java
@@ -1,12 +1,10 @@
 package com.kryptnostic.directory.v1.http;
 
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
 import com.codahale.metrics.annotation.Timed;
 import com.google.common.base.Optional;
-import com.kryptnostic.directory.v1.domain.DomainPolicy;
 import com.kryptnostic.directory.v1.model.DomainUpdate;
 import com.kryptnostic.directory.v2.model.DomainResourcesUsage;
 
@@ -27,11 +25,13 @@ import retrofit.http.Path;
  */
 public interface DomainManagementApi {
     public static final String DOMAIN               = "/domain";
-    public static final String DOMAIN_NAMES         = "/domainnames";
+
     public static final String NOTIFICATION_EMAIL   = "/notificationemail";
+
     public static final String WHITE_LIST           = "/whitelist";
+
     public static final String DOMAIN_LIST          = "/domainlist";
-    public static final String SHARING_POLICY       = "/sharingpolicy";
+
     public static final String DOMAIN_RESOURCES     = "/domainresources";
 
     public static final String NAME                 = "name";
@@ -39,6 +39,7 @@ public interface DomainManagementApi {
     public static final String RESOURCE             = "resource";
     public static final String NAME_PATH            = "/{" + NAME + "}";
     public static final String NAME_WITH_DOT        = "/{" + NAME + ":.+}";
+    public static final String SHARING_POLICY       = "/sharingpolicy";
     public static final String DOMAIN_ID_PATH       = "/{" + DOMAIN_ID + "}";
     public static final String DOMAIN_RESOURCE_PATH = "/{" + RESOURCE + "}";
 
@@ -54,19 +55,10 @@ public interface DomainManagementApi {
     
     @Timed
     @GET( DOMAIN + NAME_PATH )
-    Optional<UUID> getDomainId( @Path( NAME ) String domainName );
-    
-    @GET( DOMAIN_NAMES + DOMAIN_ID_PATH )
-    String getDomainName( @Path( DOMAIN_ID ) UUID domainUUID );
-    
-    @GET( DOMAIN_NAMES )
-    Map<UUID, String> getDomainNames( @Body Set<UUID> domainUUIDs );
+    Optional<UUID> getDomainId( @Path( NAME ) String domainName);
 
     @GET( DOMAIN + SHARING_POLICY )
-    DomainPolicy getDomainSharingPolicy();
-    
-    @POST( DOMAIN + SHARING_POLICY )
-    Response setDomainSharingPolicy( @Body DomainPolicy domainSharingPolicy );
+    String getDomainSharingPolicy();
 
     @GET( DOMAIN + NOTIFICATION_EMAIL )
     String getDomainEamilAddress();
@@ -80,19 +72,19 @@ public interface DomainManagementApi {
      * @param domains
      * @return
      */
+    @PUT( WHITE_LIST )
+    Response addToDomainWhiteList( @Body Set<String> domains );
+
     @GET( WHITE_LIST )
     Set<String> getDomainWhiteList();
-    
-    @PUT( WHITE_LIST )
-    Response addToDomainWhiteList( @Body Set<String> domainNames);
-    
+
     @DELETE( WHITE_LIST )
-    Response removeFromDomainWhiteList( @Body Set<String> domainNames );
+    Response removeFromDomainWhiteList( @Body Set<String> domains );
 
     @GET( DOMAIN_LIST )
     Set<String> getListableDomainsForWhiteList();
 
     @GET( DOMAIN_RESOURCES + DOMAIN_ID_PATH + DOMAIN_RESOURCE_PATH )
-    DomainResourcesUsage getDomainResourcesUsage( @Path( DOMAIN_ID ) UUID domainId, @Path( RESOURCE ) String resource );
+    DomainResourcesUsage getDomainResourcesUsage( @Path( DOMAIN_ID ) UUID domainId, @Path( RESOURCE ) String resource);
 
 }
