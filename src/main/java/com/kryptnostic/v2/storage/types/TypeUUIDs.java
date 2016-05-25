160de54732d24f3e621990847899e794fc827bf9
Yao <yao@kryptnostic.com>
2016-05-25 11:01:22 -0700

    fix EVENT type UUID

--- src/main/java/com/kryptnostic/v2/storage/types/TypeUUIDs.java
+++ src/main/java/com/kryptnostic/v2/storage/types/TypeUUIDs.java
@@ -12,16 +12,17 @@ public final class TypeUUIDs {
     public static final UUID UTF8_STRING            = new UUID( 0L, 1L );
     public static final UUID UTF16_STRING           = new UUID( 0L, 2L );
     public static final UUID INDEX_METADATA         = new UUID( 0L, 3L );
-    public static final UUID EVENT                  = new UUID( 0L, 4L );
 
     /**
      * A block is a reserved type for chunking a large object into several child objects.
      */
-    public static final UUID BLOCK                  = new UUID( 0L, 5L );
-    public static final UUID FHE_PRIVATE_KEY        = new UUID( 0L, 6L );
-    public static final UUID FHE_SEARCH_PRIVATE_KEY = new UUID( 0L, 7L );
+    public static final UUID BLOCK                  = new UUID( 0L, 4L );
+    public static final UUID FHE_PRIVATE_KEY        = new UUID( 0L, 5L );
+    public static final UUID FHE_SEARCH_PRIVATE_KEY = new UUID( 0L, 6L );
 
-    public static final UUID INDEX_SEGMENT          = new UUID( 0L, 8L );
+    public static final UUID INDEX_SEGMENT          = new UUID( 0L, 7L );
+
+    public static final UUID EVENT                  = new UUID( 0L, 8L );
 
     public static final UUID DEFAULT_TYPE           = UTF8_STRING;
 }
